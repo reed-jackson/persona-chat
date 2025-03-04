fc1a7a30b11bed422ba9a336c07ee72c018a7ca5
reed-jackson <reed.a.jackson@gmail.com>
2025-03-03 19:48:15 -0500

    Add hydration warning suppression

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body className={`${geist.className}`}>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 					<Theme accentColor="blue" radius="medium">
